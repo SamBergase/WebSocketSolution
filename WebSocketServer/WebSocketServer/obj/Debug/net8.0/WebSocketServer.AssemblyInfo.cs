9254d9bae8cc70be90b375f54b27016742c90fcd
SamBergase <sam.bergase@gmail.com>
2025-11-17 17:24:02 +0100

    No changes really, just tidying up

--- WebSocketServer/WebSocketServer/obj/Debug/net8.0/WebSocketServer.AssemblyInfo.cs
+++ WebSocketServer/WebSocketServer/obj/Debug/net8.0/WebSocketServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebSocketServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3701672acd991ac8dc8721a9703c6c6593d6cee7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebSocketServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebSocketServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
